4f686f11b71da3b63402fc0fd966df231a83803e
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-01-06 00:18:42 +0700

    menambahkan Icon Meterial UI

--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
+import styled from 'styled-components'
+import { HomeOutlined,SettingsOutlined,FaceOutlined} from '@material-ui/icons'
+
+
+const Container = styled.div`
+    display: flex;
+    background-color: teal;
+    height: 100vh;
+    
+`
+const Wrapper = styled.div`
+   padding: 20px;
+   background-color: chocolate;
+`
+const Menu = styled.div``
+const Title = styled.h3``
+const List = styled.ul``
+const ListItem = styled.li``
+
 
 const SideBar = () => {
     return (
-        <div>
-            sidebar
-        </div>
+        <Container>
+            <Wrapper>
+                <Menu>
+                <Title>Dashbord</Title>
+                <List>
+                    <ListItem><HomeOutlined/>home</ListItem>
+                    <ListItem><FaceOutlined/>profil</ListItem>
+                    <ListItem><SettingsOutlined/>setting</ListItem>
+                </List>
+                </Menu>
+            </Wrapper>
+        </Container>
     )
 }
 
