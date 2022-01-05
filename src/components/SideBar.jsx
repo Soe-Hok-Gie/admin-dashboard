e1ab211b79c3db5298338d9c96db3791c354e160
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-01-05 17:18:24 +0700

    membuat Sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,13 +2,38 @@ import React from 'react'
 import styled from 'styled-components'
 
 
-const Container = styled.div``;
+const Container = styled.div`
+    height : 100vh;
+    width: 100vw;
+    display: flex;
+`;
+const Wrapper = styled.div`
+    background-color: bisque;
+`;
+const Left = styled.div`
+`;
+const Logo = styled.h1``;
+const Menu = styled.ul``;
+const Menuitem = styled.li``;
+
 
 const SideBar = () => {
     return (
         <Container>
-            hello raffa babi
-        </Container>
+            <Wrapper>
+            <Left>
+                <Logo>ReactDev</Logo>
+                <Menu>
+                    <Menuitem>Home</Menuitem>
+                    <Menuitem>Project</Menuitem>
+                    <Menuitem>Billings</Menuitem>
+                    <Menuitem>Team Member</Menuitem>
+                    <Menuitem>Stats</Menuitem>
+                    <Menuitem>Settings</Menuitem>
+                </Menu>
+            </Left>
+            </Wrapper>
+        </Container>     
     )
 }
 
