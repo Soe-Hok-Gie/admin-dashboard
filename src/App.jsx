872f903ebd5308371c478e3ea4bf9d27ec147edf
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-01-05 22:02:43 +0700

    membuat routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
 import React from 'react'
-import styled from 'styled-components'
 import Header from './components/header/Header'
 import SideBar from './components/sideBar/SideBar'
 import Footer from './components/footer/Footer'
 import Profil from './components/content/Profil'
 import Setting from './components/content/Setting'
 
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  // Navigate,
 
+} from 'react-router-dom';
 
-
-const Container = styled.div``;
 
 const App = () => {
   return (
-    <Container>
-    <Header/>
-    <SideBar/>
-    <Profil/>
-    <Setting/>
-    <Footer/>
-    </Container>
+    
+    <BrowserRouter>
+      <Routes>
+         <Route path='/header' element={ <Header/>}/>
+         <Route path='/sidebar' element={ <SideBar/>}/>
+         <Route path='/profil' element={ <Profil/>}/>
+         <Route path='/setting' element={ <Setting/>}/>
+         <Route path='/footer' element={ <Footer/>}/>
+      </Routes>
+    </BrowserRouter>
+
   )
 }
 
