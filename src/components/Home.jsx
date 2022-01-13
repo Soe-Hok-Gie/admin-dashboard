e76cef030da8ba3be8d22c69311effb9c2e2b044
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-01-13 23:40:36 +0700

    membuat component Container, wrapperSidebar, wrappermain dan membuat stylingnya dgn membagi dua halaman

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Container = styled.div``;
+const Container = styled.div`
+    width: 100vw;
+    height: auto;
+    background-color: gray;
+    display: flex;
+`;
+const WrapperSidebar = styled.div`
+    height: 100vh;
+    width: 300px;
+    background-color: teal;
+`;
+const WrapperMain = styled.div`
+    height:auto;
+    background-color:cornsilk;
+    width: 100%;
+`;
 
 const Home = () => {
     return (
         <Container>
-          home tes
+        <WrapperSidebar>
+
+        </WrapperSidebar>
+        <WrapperMain>
+
+        </WrapperMain>
         </Container>
     )
 }
